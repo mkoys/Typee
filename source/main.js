4f1892879d647283252305af6901f9aaeca235f6
mkoys <mkoys@protonmail.com>
2023-04-02 21:23:24 +0200

    Menu search
    Created html & css markup for seach field in popup menu.

--- source/main.js
+++ source/main.js
@@ -15,8 +15,8 @@ const wordNumber = 30;
 let menu = false;
 let text = "";
 let position = 0;
-let seed = 0;
-const ignoreKeys = ["Shift", "Tab", "Alt", "Control", "Delete", "Enter", "CapsLock", "Home", "Insert", "PageUp", "PageDown", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "Pause", "ScrollLock", "PrintScreen", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", ];
+let seed = 666;
+const ignoreKeys = ["Shift", "Tab", "Alt", "Control", "Delete", "Enter", "CapsLock", "Home", "Insert", "PageUp", "PageDown", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "Pause", "ScrollLock", "PrintScreen", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "Meta", "Dead" ];
 
 popupBoxElement.style.visibility = "hidden";
 popupBoxElement.style.opacity = 0;
@@ -127,6 +127,8 @@ document.addEventListener("keydown", (event) => {
 		}
     return;
 	}
+
+	if(menu) return;
 
 	const previousCharacterElement = map[position];
 	
