4804c948796a7ac8762e786595d7a89846e3412f
mkoys <mkoys@protonmail.com>
2023-10-23 15:34:51 +0200

    Menu
    By pressing escape you can now enter a menu where you can choose
    from options and setup the typing test for your liking. Currenly there
    is word count, language and font size settings in the menu. You can
    travers the menu eather by using mouse and clicking the option you want
    to choose or by typing and searching for it and confirming it by
    pressing enter, you can also cycle through the list by pressing up and
    down arrow keys.

--- source/main.js
+++ source/main.js
@@ -3,15 +3,36 @@ import words from "./words.js"
 const mainElement = document.querySelector(".main"); 
 const textElement = document.querySelector(".text");
 const cursorElement = document.querySelector(".cursor");
+const popUpElement = document.querySelector(".popup");
+const menuSearchElement = document.querySelector(".menuInput");
+const menuOptionsElement = document.querySelector(".menuOptions");
 
-const textLength = 20;
-const fontSize = 24;
 const fontFamily = "Cousine";
-const textLanguage = "english";
+const arrowKeys = ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"];
 const funtionKeys = ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12"];
-const ignoreKeys = ["Shift", "Alt", "CapsLock", "Controll", "Enter", "Delete", "Insert", "Home", "End", "PageUp", "PageDown", "ScrollLock", "Pause", ...funtionKeys];
+const ignoreKeys = ["Shift", "Alt", "CapsLock", "Enter", "Controll", "Delete", "Insert", "Home", "End", "PageUp", "PageDown", "ScrollLock", "Pause", ...funtionKeys, ...arrowKeys];
 const visibleLines = 3;
 
+const mainMenu = [
+  {
+    name: "Language...",
+    action: () => menuLanguage()
+  },
+  {
+    name: "Font Size...",
+    action: () => menuFontSize()
+  },
+  {
+    name: "Word Count...",
+    action: () => menuWordCount()
+  }
+];
+
+let textLength = 20;
+let textLanguage = "english";
+let menuOptions = [];
+let menuSelected = 0;
+let fontSize = 24;
 let text = "";
 let cursorCss;
 let characterWidth;
@@ -24,52 +45,198 @@ let characterHeight = 0;
 let right = 0;
 let wrong = 0;
 let timer = null;
+let menuOpen = false;
 
 calibrate();
+loadMenu({options: mainMenu});
 generateText(textLength);
 renderText(text);
 
 const resizeObserver = new ResizeObserver(_entries => updateView({ force: true }));
 resizeObserver.observe(textElement);
 
-document.addEventListener("keydown", event => {
+menuSearchElement.addEventListener("keyup", event => {
   if(ignoreKeys.findIndex(key => key === event.key) > -1) return;
+  const filter = menuSearchElement.value;
+  loadMenu({filter});
+});
+
+document.addEventListener("keydown", event => {
   if(!timer) timer = performance.now();
-  if(event.key === "Tab") {
-    event.preventDefault();
-    reset();
-    generateText(textLength);
-    renderText(text);
-  }else if(event.key === "Backspace") {
-    cursorBack();
-    const currentCharacter = textElement.children[wordPosition].children[characterPosition];
-    if(currentCharacter.classList.contains("wrong")) wrong--;
-    if(currentCharacter.classList.contains("right")) right--;
-    currentCharacter.classList.remove("wrong");
-    currentCharacter.classList.remove("right");
-  }else {
-    const currentCharacter = textElement.children[wordPosition].children[characterPosition];
-    if(currentCharacter.textContent === event.key) {
-      currentCharacter.classList.add("right");
-      right++;
-    }else {
-      currentCharacter.classList.add("wrong");
-      wrong++;
+  if(event.key === "Escape") return openMenu();
+  if(event.key === "ArrowUp" && menuOpen) menuSelect(menuSelected - 1);
+  if(event.key === "ArrowDown" && menuOpen) menuSelect(menuSelected + 1);
+  if(event.key === "Enter") {
+    if(menuOpen) {
+      const filter = menuSearchElement.value;
+      const filteredOptions = [];
+      menuOptions.forEach(option => { if(option.name.toLowerCase().startsWith(filter.toLowerCase())) filteredOptions.push(option) });
+      filteredOptions[menuSelected].action();
     }
-    cursorNext();
+  }
+
+  if(ignoreKeys.findIndex(key => key === event.key) > -1) return;
+  if(!menuOpen) {
+    if(event.key === "Tab") {
+      event.preventDefault();
+      reset();
+      generateText(textLength);
+      renderText(text);
+    }else if(event.key === "Backspace") {
+      cursorBack();
+      const currentCharacter = textElement.children[wordPosition].children[characterPosition];
+      if(currentCharacter.classList.contains("wrong")) wrong--;
+      if(currentCharacter.classList.contains("right")) right--;
+      currentCharacter.classList.remove("wrong");
+      currentCharacter.classList.remove("right");
+    }else {
+      const currentCharacter = textElement.children[wordPosition].children[characterPosition];
+      if(currentCharacter.textContent === event.key) {
+        currentCharacter.classList.add("right");
+        right++;
+      }else {
+        currentCharacter.classList.add("wrong");
+        wrong++;
+      }
+      cursorNext();
+    }
   }
 });
 
+function openMenu(action) {
+  if(action === undefined) {
+    menuOpen ? close() : open();
+  }else {
+    action ? open() : close();
+  }
+
+  function open() {
+    menuOpen = true;
+    popUpElement.style.visibility = "visible";
+    popUpElement.style.opacity = "1";
+    setTimeout(() => menuSearchElement.focus(), 10);
+  }
+
+  function close() {
+    menuOpen = false;
+    menuSelected = 0;
+    popUpElement.style.visibility = null;
+    popUpElement.style.opacity = null;
+    setTimeout(() => loadMenu({options: mainMenu}), 200);
+  }
+}
+
+function menuLanguage() {
+  const languages = Object.keys(words);
+  const languageMenu = languages.map(language => { 
+    return { name: language.charAt(0).toUpperCase() + language.slice(1), action: () => { 
+      textLanguage = language; 
+      reset();
+      generateText(textLength);
+      renderText(text);
+      openMenu(false);
+      setTimeout(() => loadMenu({options: mainMenu}), 200);
+    }}
+  });
+
+  loadMenu({options: languageMenu});
+}
+
+function menuFontSize() {
+  const changeFont = (newfontSize) => {
+    fontSize = newfontSize; 
+    reset();
+    calibrate();
+    generateText(textLength);
+    openMenu(false);
+    setTimeout(() => loadMenu({options: mainMenu}), 200);
+  }
+
+  const fontMenu = [
+    { name: "12px", action: () => changeFont(12) },
+    { name: "14px", action: () => changeFont(14) },
+    { name: "16px", action: () => changeFont(16) },
+    { name: "20px", action: () => changeFont(20) },
+    { name: "24px", action: () => changeFont(24) }
+  ];
+
+  loadMenu({options: fontMenu});
+}
+
+function menuWordCount() {
+  const changeCount = (newCount) => {
+    textLength = newCount; 
+    reset();
+    generateText(textLength);
+    renderText(text);
+    openMenu(false);
+    setTimeout(() => loadMenu({options: mainMenu}), 200);
+  }
+
+  const countMenu = [
+    { name: "3 words", action: () => changeCount(3) },
+    { name: "5 words", action: () => changeCount(5) },
+    { name: "10 words", action: () => changeCount(10) },
+    { name: "20 words", action: () => changeCount(20) },
+    { name: "50 words", action: () => changeCount(50) }
+  ];
+
+  loadMenu({options: countMenu});
+}
+
+function menuSelect(index) {
+  if(menuOptionsElement.children.length > 0) {
+    if(menuOptionsElement.children.length > menuSelected && menuSelected > -1) menuOptionsElement.children[menuSelected].classList.remove("selected");
+    menuSelected = index;
+    if(menuOptionsElement.children.length > menuSelected && menuSelected >= 0) {
+      menuOptionsElement.children[menuSelected].classList.add("selected");
+    }else if(menuSelected < 0) {
+      menuSelect(menuOptionsElement.children.length - 1);
+    }else {
+      menuSelect(0);
+    }
+  }else {
+    menuSelected = 0;
+  }
+}
+
+function loadMenu({ options, filter }) {
+  if(!filter) filter = "";
+  if(options) {
+    menuOptions = options;
+    menuSearchElement.value = "";
+    setTimeout(() => menuSearchElement.focus(), 10);
+  }
+
+  menuOptionsElement.innerHTML = "";
+  menuOptions.forEach((option, optionIndex) => {
+    if(option.name.toLowerCase().startsWith(filter.toLowerCase())) {
+      const menuOptionElement = document.createElement("div");
+      const menuOptionTextElement = document.createElement("p");
+      if(optionIndex == menuSelected) menuOptionElement.classList.add("selected");
+      menuOptionElement.classList.add("menuOption");
+      menuOptionTextElement.classList.add("menuOptionText");
+      menuOptionTextElement.textContent = option.name;
+      menuOptionElement.appendChild(menuOptionTextElement);
+      menuOptionsElement.appendChild(menuOptionElement);
+      menuOptionElement.addEventListener("click", _event => option.action());
+      menuOptionElement.addEventListener("mouseenter", _event => menuSelect(optionIndex));
+    }
+  });
+
+  menuSelect(0);
+}
+
 function reset() {
-    text = "";
-    right = 0;
-    wrong = 0;
-    wordPosition = 0;
-    characterPosition = 0;
-    updateView({ force: true });
+  right = 0;
+  wrong = 0;
+  wordPosition = 0;
+  characterPosition = 0;
+  updateView({ force: true });
 }
 
 function calibrate() {
+  characterHeight = 0;
   const canvas = new OffscreenCanvas(100, 100);
   const cssRules = document.styleSheets[0].cssRules;
   const context = canvas.getContext("2d");
@@ -103,6 +270,7 @@ function calibrate() {
 }
 
 function generateText(length = 0) {
+  text = "";
   for(let index = 0; index < length; index++) {
     const randomNumber = Math.floor(Math.random() * words[textLanguage].length);
     text += index != length - 1 ? `${words[textLanguage][randomNumber]} ` : words[textLanguage][randomNumber];
