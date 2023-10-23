f9b40765630f911679de36179be6af78e24db834
mkoys <mkoys@protonmail.com>
2023-10-23 20:22:26 +0200

    Server
    Refactored server to use only native Node.js modules to create and host
    application with a HTTP server.

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
+import { createServer } from "http";
+import { lstat, readdir, readFile } from "fs";
+import { join, extname } from "path";
 
 const port = 3000;
-const app = express();
+const currentPath = new URL("./source", import.meta.url).pathname;
+const server = createServer(requestHandler);
+const mimeTypes = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".json": "application/json",
+  ".png": "image/png",
+  ".gif": "image/gif",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg"
+};
 
-app.use(morgan("tiny"));
-app.use(helmet());
-app.use(cors());
-app.use(express.static("source"));
+async function requestHandler(request, response) {
+  const requestPath = join(currentPath, request.url);
+  const mimeType = mimeTypes[extname(requestPath)];
+  if(mimeType) response.writeHead(200, { "Content-type": mimeType });
+  const pathItem = await loadPath(requestPath);
+  response.end(pathItem);	
+}
 
-app.listen(port, () => console.log(`Running on http://localhost:${port}`));
+async function loadPath(path) {
+  return new Promise((resolve) => {
+    lstat(path, async (error, stats) => {
+      if(error) return resolve(error.toString());
+      if(stats.isDirectory()) {
+        const directory = await directoryRead(path);
+        resolve(directory);
+      }else {
+        const file = await fileRead(path);
+        resolve(file);
+      }
+    });
+  });
+} 
+
+function fileRead(path) {
+  return new Promise((resolve) => {
+    readFile(path, "utf-8", (error, data) => {
+      if(error) return resolve(error.toString());
+      resolve(data);
+    });
+  });
+}
+
+function directoryRead(path) {
+  return new Promise((resolve) => {
+    readdir(path, (error, items) => {
+      if(error) return resolve(error.toString());
+      resolve(items.join("\n"));
+    });
+  });
+}
+
+server.listen(port, () => console.log(`Server running on http://localhost:${port}`));
